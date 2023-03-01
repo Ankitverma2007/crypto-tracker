7a668985a75535f8c71777fc6c90ca56e52d47cf
Ankitverma2007 <ankitvermarapu@gmail.com>
2023-03-01 12:38:40 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import {Router, Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
+import Sidebar from "./Comonents/Sidebar";
 import Dashboard from "./pages/Dashboard";
 import Exchanges from "./pages/Exchanges";
+import "./Style/_styles.scss"
 
 function App() {
   return (
     <div className="App">
+      <Sidebar/>
       <Routes>
         <Route path="/" element={<Dashboard/>}/>
         <Route path="/exchanges" element={<Exchanges/>}/>
---
+++ src/Comonents/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const Sidebar = () => {
+  return (
+<aside className='sidebar'>
+<div className="logo">
+    <h2>Crypto Tracker</h2>
+</div>
+<Link to="/">
+  <i className='fa-solid fa-server'></i> Dashboard</Link>
+<Link to="/exchanges">
+    <i className='fa-solid fa-chart-line'></i> Exchanges</Link>
+</aside>
+    )
+}
+
+export default Sidebar
